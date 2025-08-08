c02ee954b0fdc76526edccae6a303a696455a44d
itsskofficial <itsskofficial03@gmail.com>
2025-08-08 09:11:35 +0530

    feat: pwa setup for mobile

--- src/client/app/layout.js
+++ src/client/app/layout.js
@@ -62,7 +62,8 @@ export default function RootLayout({ children }) {
 					sizes="16x16"
 					href="/favicon-16x16.png"
 				/>
-				<link rel="manifest" href="/site.webmanifest" />
+				<meta name="theme-color" content="#000000" />
+				<link rel="manifest" href="/manifest.json" />
 			</head>
 			<body className="font-sans">
 				<Auth0Provider>
